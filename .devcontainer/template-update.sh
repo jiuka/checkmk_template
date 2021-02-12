7e7400f3cba88c1667f84d0b02b4c1b2894859f6
Marius Rieder <marius.rieder@durchmesser.ch>
2021-02-12 13:28:48 +0100

    Implement template sync script

---
+++ .devcontainer/template-update.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+TEMPDIR=$(mktemp --directory)
+
+cleanup() {
+  echo "Removing $TEMPDIR"
+  rm -rf $TEMPDIR
+}
+trap cleanup EXIT
+
+git -C $TEMPDIR clone https://github.com/jiuka/checkmk_template.git
+
+CMD="rsync --archive --cvs-exclude --no-owner --no-group --no-times --verbose"
+if [ -e ".devcontainer/template-sync.conf" ]; then
+    CMD="${CMD} --filter='merge .devcontainer/template-sync.conf'"
+fi
+if [ -e "${TEMPDIR}/checkmk_template/.devcontainer/template-sync-includes.conf" ]; then
+    CMD="${CMD} --filter='merge ${TEMPDIR}/checkmk_template/.devcontainer/template-sync-includes.conf'"
+fi
+CMD="${CMD} --filter='exclude *' ${TEMPDIR}/checkmk_template/ $(pwd)/"
+bash -c "$CMD"
+
+echo $CMD
