ac6f7881570817a1616ca5033047ea9195bc986d
Marius Rieder <marius.rieder@durchmesser.ch>
2023-06-16 17:04:14 +0200

    Update for Checkmk 2.2.0

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-NAME=$(python -c 'print(eval(open("package").read())["name"])')
+NAME=$(python3 -c 'print(eval(open("package").read())["name"])')
 rm /omd/sites/cmk/var/check_mk/packages/* ||:
-ln -s $WORKSPACE/package /omd/sites/cmk/var/check_mk/packages/$NAME
+rm /omd/sites/cmk/var/check_mk/packages_local/* ||:
 
-mkp -v pack $NAME
+mkp -v package package ||:
+cp /omd/sites/cmk/var/check_mk/packages_local/*.mkp .
 
 # Set Outputs for GitHub Workflow steps
 if [ -n "$GITHUB_WORKSPACE" ]; then
-    echo "::set-output name=pkgfile::$(ls *.mkp)"
-    echo "::set-output name=pkgname::${NAME}"
-    VERSION=$(python -c 'print(eval(open("package").read())["version"])')
-    echo "::set-output name=pkgversion::$VERSION"
+    echo "pkgfile=$(ls *.mkp)" >> $GITHUB_OUTPUT
+    echo "pkgname=${NAME}" >> $GITHUB_OUTPUT
+    VERSION=$(python3 -c 'print(eval(open("package").read())["version"])')
+    echo "pkgversion=$VERSION" >> $GITHUB_OUTPUT
 fi
