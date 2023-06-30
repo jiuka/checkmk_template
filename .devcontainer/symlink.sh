73620128e75bbad487cb23ecc844292d82d28cad
Marius Rieder <marius.rieder@durchmesser.ch>
2023-06-30 21:45:07 +0200

    Update symlink for new Checkmk Bakery plugin path

--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -5,8 +5,11 @@ for DIR in 'agents' 'checkman' 'checks' 'doc' 'inventory' 'notifications' 'pnp-t
     ln -sv $WORKSPACE/$DIR $OMD_ROOT/local/share/check_mk/$DIR
 done;
 
-rm -rfv $OMD_ROOT/local/lib/check_mk/base/plugins/agent_based
-ln -sv $WORKSPACE/agent_based $OMD_ROOT/local/lib/check_mk/base/plugins/agent_based
+rm -rfv $OMD_ROOT/local/lib/python3/cmk/base/plugins/agent_based
+ln -sv $WORKSPACE/agent_based $OMD_ROOT/local/lib/python3/cmk/base/plugins/agent_based
+
+mkdir -p $OMD_ROOT/local/lib/python3/cmk/base/cee/plugins
+ln -sv $WORKSPACE/bakery $OMD_ROOT/local/lib/python3/cmk/base/cee/plugins/bakery
 
 rm -rfv $OMD_ROOT/local/lib/nagios/plugins
 ln -sv $WORKSPACE/nagios_plugins $OMD_ROOT/local/lib/nagios/plugins
