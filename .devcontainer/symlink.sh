b40660dc480167c9b424a11ea424c6dcb9459a72
Marius Rieder <marius.rieder@durchmesser.ch>
2024-06-05 18:35:33 +0200

    Rework for Checkmk 2.3.0

--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+PKGNAME=$(python3 -c 'print(eval(open("package").read())["name"])')
+ln -sv $WORKSPACE $OMD_ROOT/local/lib/python3/cmk_addons/plugins/$PKGNAME
 
 for DIR in 'agents' 'checkman' 'checks' 'doc' 'inventory' 'notifications' 'pnp-templates' 'web'; do
     rm -rfv $OMD_ROOT/local/share/check_mk/$DIR
